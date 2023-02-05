9a945c6ce3e9d83c646c5b6f2590d04259af337c
Hanselkek <87748222+Hanselkek@users.noreply.github.com>
2023-02-05 09:58:05 +0800

    first commit

---
+++ o.lua
@@ -0,0 +1,4 @@
+local n1 = 5
+local n2 = 5
+-- Funcs
+print(n1 + n2)
---
+++ scotch.py
@@ -0,0 +1,24 @@
+FILE_PATH = "o.lua"
+
+current_line = 1
+lines = []
+
+def write_up():
+    global current_line
+
+    c = str(input(f"[{current_line}]: "))
+
+    if c.lower() != "stop":
+        with open(f"{FILE_PATH}", "w") as f:
+            lines.append(c + "\n")
+            f.write("")
+            f.writelines(lines)
+            f.close()
+
+            current_line += 1
+            write_up()
+    else:
+        lines.clear()
+        return
+
+write_up()
