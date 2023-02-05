bd5db33a41119a087f45022a7f5f16ded19a7fa5
Hanselkek <87748222+Hanselkek@users.noreply.github.com>
2023-02-05 20:50:04 +0800

    added psutil_instance

---
+++ psutil_instance.py
@@ -0,0 +1,8 @@
+import os
+import psutil
+
+def get_mem_usage_by_app():
+    pid = os.getpid()
+    python_process = psutil.Process(pid)
+    memoryUse = python_process.memory_info()[0]/2.**30
+    return memoryUse
